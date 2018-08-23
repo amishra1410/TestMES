4741e9802822a3e8295f36b7f534023d084c46ff
Chinmay Trivedi <chinmay.trivedi@algoma.com>
2018-08-23 12:56:33 -0400

    Testing of first commit

--- Test_App/Controllers/HomeController.cs
+++ Test_App/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Test_App.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page.- Change by Chinmay";
 
             return View();
         }
